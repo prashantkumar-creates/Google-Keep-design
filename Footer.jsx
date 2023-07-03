50b0d5e2350b06fefbe5d8746853fbbf66a0e74f
prashant kumar <100068854+prashantkumar-creates@users.noreply.github.com>
2023-07-03 11:19:25 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+function Footer(){
+  let year = new Date().getFullYear();
+  return (
+     <footer>
+       <p>
+    Copyright ©️{year}
+  </p>
+     </footer>
+  );
+}
+
+
+export default Footer;
---
+++ Note.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Note(){
+  return (
+    <div className="note">
+      <h1>Prashant</h1>
+     
+      <p>Hope u will like it</p>
+    </div>
+  )
+}
+
+export default Note;
